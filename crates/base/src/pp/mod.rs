864bd05c856be3140ad5d70a6fbd2c77df35f1cd
ryn <ryn@fppf.site>
2025-07-20 13:59:36 -0400

    Update pretty printer with Justin Pombrio's twist

--- crates/base/src/pp/mod.rs
+++ crates/base/src/pp/mod.rs
@@ -2,18 +2,23 @@
 use std::{
     borrow::Cow,
     cell::Cell,
-    collections::VecDeque,
     fmt::{self, Write},
     io,
 };
 
 use crate::arena::TypedArena;
 
-pub const INDENT: isize = 3;
-pub const PRETTY_WIDTH: usize = 80;
+pub const INDENT: u32 = 3;
+pub const PRETTY_WIDTH: u32 = 80;
 
 pub trait WritePretty {
-    fn pretty_print<W: io::Write>(&self, width: usize, w: &mut W) -> io::Result<()>;
+    fn pretty_print<W: io::Write>(&self, width: u32, w: &mut W) -> io::Result<()>;
+
+    fn pretty_string(&self, width: u32) -> String {
+        let mut v = Vec::new();
+        self.pretty_print(width, &mut v).unwrap();
+        String::from_utf8(v).unwrap()
+    }
 }
 
 pub struct Pretty<T>(pub T);
@@ -23,9 +28,7 @@ where
     T: WritePretty,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut v = Vec::new();
-        self.0.pretty_print(PRETTY_WIDTH, &mut v).unwrap();
-        String::from_utf8(v).unwrap().fmt(f)
+        self.0.pretty_string(PRETTY_WIDTH).fmt(f)
     }
 }
 
@@ -121,17 +124,18 @@ where
     }
 }
 
-/// Strictly Pretty <https://lindig.github.io/papers/strictly-pretty-2000.pdf>.
-/// API design from <https://github.com/Marwes/pretty.rs>
+/// Impl from <https://justinpombrio.net/2024/02/23/a-twist-on-Wadlers-printer.html>.
+/// See also Strictly Pretty <https://lindig.github.io/papers/strictly-pretty-2000.pdf>.
+/// API design from <https://github.com/Marwes/pretty.rs>.
 #[derive(Clone, Debug)]
 pub enum Doc<'a> {
     Empty,
     Line,
     Text(Cow<'a, str>),
-    Break(&'a str),
-    Nest(isize, &'a Doc<'a>),
-    Append(&'a Doc<'a>, &'a Doc<'a>),
+    Nest(u32, &'a Doc<'a>),
     Group(&'a Doc<'a>),
+    Append(&'a Doc<'a>, &'a Doc<'a>),
+    Choice(&'a Doc<'a>, &'a Doc<'a>),
 }
 
 pub trait IntoDoc<'a> {
@@ -152,12 +156,8 @@ impl<'a> DocArena<'a> {
         DocBuilder(self, Doc::Text(s.into()))
     }
 
-    pub fn brk(&'a self, s: &'a str) -> DocBuilder<'a> {
-        DocBuilder(self, Doc::Break(s))
-    }
-
     pub fn space(&'a self) -> DocBuilder<'a> {
-        self.brk(" ")
+        self.text(" ")
     }
 
     pub fn empty(&'a self) -> DocBuilder<'a> {
@@ -226,7 +226,12 @@ impl<'a> DocBuilder<'a> {
         DocBuilder(alloc, Doc::Append(self.doc(), other.into_doc(alloc).doc()))
     }
 
-    pub fn nest(self, indent: isize) -> DocBuilder<'a> {
+    pub fn choice<D: IntoDoc<'a>>(self, other: D) -> DocBuilder<'a> {
+        let alloc = self.0;
+        DocBuilder(alloc, Doc::Choice(self.doc(), other.into_doc(alloc).doc()))
+    }
+
+    pub fn nest(self, indent: u32) -> DocBuilder<'a> {
         DocBuilder(self.0, Doc::Nest(indent, self.doc()))
     }
 
@@ -243,107 +248,188 @@ impl<'a> DocBuilder<'a> {
         self.append(space).append(other)
     }
 
-    pub fn pretty<W: io::Write>(self, writer: &mut W, width: usize) -> io::Result<()> {
-        let docs = VecDeque::from([(0, Mode::Flat, self.0.alloc(self.1))]);
-        format(writer, width as isize, 0, docs)
+    pub fn pretty<W: io::Write>(self, writer: &mut W, width: u32) -> io::Result<()> {
+        let mut printer = Printer::new(self.doc(), width);
+        printer.format(writer)
     }
 
-    pub fn pretty_string(self, width: usize) -> String {
+    pub fn pretty_string(self, width: u32) -> String {
         let mut buffer = Vec::new();
         self.pretty(&mut buffer, width).unwrap();
         String::from_utf8(buffer).unwrap()
     }
 }
 
+struct Printer<'a> {
+    width: u32,
+    col: u32,
+    chunks: Vec<Chunk<'a>>,
+}
+
 #[derive(Clone, Copy)]
-pub enum Mode {
-    Flat,
-    Break,
+struct Chunk<'a> {
+    doc: &'a Doc<'a>,
+    indent: u32,
+    flat: bool,
 }
 
-fn fits(width: isize, mut docs: VecDeque<(isize, Mode, &Doc<'_>)>) -> bool {
-    if width < 0 {
-        return false;
-    }
-    let mut total = 0isize;
-    while total <= width
-        && let Some((indent, mode, doc)) = docs.pop_front()
-    {
-        match doc {
-            Doc::Empty => continue,
-            Doc::Line => return true,
-            Doc::Text(s) => total += s.len() as isize,
-            Doc::Break(s) => match mode {
-                Mode::Flat => total += s.len() as isize,
-                Mode::Break => return true,
-            },
-            Doc::Nest(inner_indent, doc) => docs.push_front((indent + inner_indent, mode, doc)),
-            Doc::Append(l, r) => {
-                docs.push_front((indent, mode, r));
-                docs.push_front((indent, mode, l));
-            }
-            Doc::Group(doc) => {
-                docs.push_front((indent, Mode::Flat, doc));
-            }
+impl<'a> Chunk<'a> {
+    fn with_doc(self, doc: &'a Doc<'a>) -> Self {
+        Self {
+            doc,
+            indent: self.indent,
+            flat: self.flat,
         }
     }
-    total <= width
+
+    fn indent(self, indent: u32) -> Self {
+        Self {
+            doc: self.doc,
+            indent: self.indent + indent,
+            flat: self.flat,
+        }
+    }
+
+    fn flat(self) -> Self {
+        Self {
+            doc: self.doc,
+            indent: self.indent,
+            flat: true,
+        }
+    }
 }
 
-fn format<W: io::Write>(
-    writer: &mut W,
-    width: isize,
-    mut consumed: isize,
-    mut docs: VecDeque<(isize, Mode, &Doc<'_>)>,
-) -> io::Result<()> {
-    while let Some((indent, mode, doc)) = docs.pop_front() {
-        match doc {
-            Doc::Empty => (),
-            Doc::Text(s) => {
-                writer.write_all(s.as_bytes())?;
-                consumed += s.len() as isize;
-            }
-            Doc::Line => {
-                write_indent(writer, indent)?;
-                consumed = indent;
-            }
-            Doc::Break(s) => {
-                writer.write_all(s.as_bytes())?;
-                consumed = match mode {
-                    Mode::Flat => consumed + s.len() as isize,
-                    Mode::Break => {
-                        write_indent(writer, indent)?;
-                        indent
+impl<'a> Printer<'a> {
+    fn new(doc: &'a Doc<'a>, width: u32) -> Self {
+        Self {
+            width,
+            col: 0,
+            chunks: vec![Chunk {
+                doc,
+                indent: 0,
+                flat: false,
+            }],
+        }
+    }
+
+    fn fits(&self, chunk: Chunk<'a>) -> bool {
+        let mut remaining = if self.col <= self.width {
+            self.width - self.col
+        } else {
+            return false;
+        };
+        let mut stack = vec![chunk];
+        let mut chunks = &self.chunks as &[Chunk];
+        loop {
+            let chunk = match stack.pop() {
+                Some(chunk) => chunk,
+                None => match chunks.split_last() {
+                    None => return true,
+                    Some((chunk, more_chunks)) => {
+                        chunks = more_chunks;
+                        *chunk
                     }
-                };
-            }
-            Doc::Nest(inner_indent, doc) => {
-                docs.push_front((indent + inner_indent, mode, doc));
-            }
-            Doc::Append(l, r) => {
-                docs.push_front((indent, mode, r));
-                docs.push_front((indent, mode, l));
-            }
-            Doc::Group(doc) => {
-                let mode = if fits(
-                    width - consumed,
-                    VecDeque::from([(indent, Mode::Flat, *doc)]),
-                ) {
-                    Mode::Flat
-                } else {
-                    Mode::Break
-                };
-                docs.push_front((indent, mode, doc));
+                },
+            };
+
+            match chunk.doc {
+                Doc::Empty => continue,
+                Doc::Line => return true,
+                Doc::Text(s) => {
+                    let width = s.len() as u32;
+                    if width <= remaining {
+                        remaining -= width;
+                    } else {
+                        return false;
+                    }
+                }
+                Doc::Nest(i, d) => stack.push(chunk.with_doc(d).indent(*i)),
+                Doc::Group(d) => stack.push(chunk.with_doc(d).flat()),
+                Doc::Append(l, r) => {
+                    stack.push(chunk.with_doc(r));
+                    stack.push(chunk.with_doc(l));
+                }
+                Doc::Choice(l, r) => {
+                    if chunk.flat {
+                        stack.push(chunk.with_doc(l));
+                    } else {
+                        // Relies on the rule that for every choice `x | y`,
+                        // the first line of `y` is no longer than the first
+                        // line of `x`.
+                        stack.push(chunk.with_doc(r));
+                    }
+                }
             }
         }
     }
-    Ok(())
+
+    fn format<W: io::Write>(&mut self, writer: &mut W) -> io::Result<()> {
+        while let Some(chunk) = self.chunks.pop() {
+            match chunk.doc {
+                Doc::Empty => (),
+                Doc::Text(s) => {
+                    writer.write_all(s.as_bytes())?;
+                    self.col += s.len() as u32;
+                }
+                Doc::Line => {
+                    writer.write_all(b"\n")?;
+                    for _ in 0..chunk.indent {
+                        writer.write_all(b" ")?;
+                    }
+                    self.col = chunk.indent;
+                }
+                Doc::Nest(i, d) => self.chunks.push(chunk.with_doc(d).indent(*i)),
+                Doc::Group(d) => self.chunks.push(chunk.with_doc(d).flat()),
+                Doc::Append(l, r) => {
+                    self.chunks.push(chunk.with_doc(r));
+                    self.chunks.push(chunk.with_doc(l));
+                }
+                Doc::Choice(l, r) => {
+                    if chunk.flat || self.fits(chunk.with_doc(l)) {
+                        self.chunks.push(chunk.with_doc(l))
+                    } else {
+                        self.chunks.push(chunk.with_doc(r))
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
-fn write_indent<W: io::Write>(writer: &mut W, indent: isize) -> Result<(), io::Error> {
-    writer.write_all(b"\n")?;
-    for _ in 0..indent {
-        writer.write_all(b" ")?;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn indent() {
+        let arena = DocArena::default();
+        let singleline = arena
+            .text("hello")
+            .append(",")
+            .space("world")
+            .enclose("(", ")")
+            .group();
+        let multiline = arena
+            .line()
+            .append("hello")
+            .append(",")
+            .append(arena.line())
+            .append("world")
+            .append(",")
+            .nest(4)
+            .append(arena.line())
+            .enclose("(", ")")
+            .group();
+        let doc = singleline.choice(multiline);
+
+        assert_eq!("(hello, world)", doc.clone().pretty_string(100));
+        assert_eq!(
+            r#"(
+    hello,
+    world,
+)"#,
+            doc.pretty_string(10)
+        );
     }
-    Ok(())
 }
